060701d7d6ff85b084c671e415f81deccb253387
KilMer56 <mer.killian56@gmail.com>
2019-11-05 15:43:55 +0100

    End of test -> Need to be checked

--- SeleniumTests/src/TestSelenium.java
+++ SeleniumTests/src/TestSelenium.java
@@ -1,6 +1,10 @@
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import org.junit.Assert;  
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.*;
 
@@ -11,48 +15,56 @@ public class TestSelenium {
 		//setting the driver executable
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Mer\\Documents\\Selenium\\chromedriver.exe");
 		
-		//test1();
-		//test2();
+		test1();
+		test2();
 		test7();
 	}
 	
 	private static void test1() {
 		WebDriver driver = new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		
 		driver.get("https://www.imt-atlantique.fr");
 		
 		WebElement link = driver.findElement(By.xpath("//a[text()='Toutes les actualités']"));
 		
-		System.out.println(link.getAttribute("href"));
+		Assert.assertNotNull(link);
 		
 		driver.close();
 	}
 	
 	private static void test2() {
 		WebDriver driver = new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		//WebDriverWait wait = new WebDriverWait(driver, 30);
 		
 		driver.get("https://www.imt-atlantique.fr");
 		
-		driver.findElement(By.xpath("//a[text()='Toutes les actualités']")).click();
+		//wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[text()='Toutes les actualités']"))).click();
+		
+		WebElement click = driver.findElement(By.xpath("//a[text()='Toutes les actualités']"));
+		click.click();
 		
 		WebElement link = driver.findElement(By.xpath("//a[text()='Accueil']"));
 		
-		System.out.println(link.getAttribute("href"));
+		Assert.assertNotNull(link);
 		
 		driver.close();
 	}
 	
 	private static void test7() {
 		WebDriver driver = new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		
 		driver.get("https://www.imt-atlantique.fr");
 		
-		List<WebElement> elems = driver.findElements(By.xpath("//*[@class='actu_home_ctner_inner_cell1_titre']"));
+		String text = driver.findElements(By.xpath("//*[@class='actu_home_ctner_inner_cell1_titre']")).get(0).getText();
+		driver.findElements(By.xpath("//*[@class='actu_home_ctner_inner_cell1_titre']/parent::a")).get(0).click();
 		
-		System.out.println(elems);
+		WebElement find = driver.findElement(By.xpath("//title"));
+		
+		Assert.assertNotNull(find);
+		Assert.assertTrue(find.getText().contains(text));
 		
 		driver.close();
 	}
