54e6dd6315cdefa24f8f71bc0dd2c5276e6231c9
Lb-lea <happyvivus@gmail.com>
2019-11-24 18:28:17 +0100

    src-gen

--- SeleniumWebdriverTests/src-gen/BrokenTestsTest.java
+++ SeleniumWebdriverTests/src-gen/BrokenTestsTest.java
@@ -4,103 +4,73 @@ public class brokenTestsTest {
 
 	public static void main(String[] args) { 
 		
-		case3();
 		case4();
 		case5();
 		case6();
-		case7();
-		case8();
 					
-	} 
-	
-	private static void  case3() {
-		WebDriver driver = new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-		
-		driver.
-		WebElement link1 = driver.findElement(By.xpath("//a[label()='Toutes les actualités']"))
-		link1.click();
-		WebElement image2 = driver.findElement(By.xpath("//*[alt()='Accueil']"))
-		image2.click();
-		WebElement link3 = driver.findElement(By.xpath("//a[text()='Toutes les actualités']"));
-		Assert.assertNotNull(link);
-		driver.close();
 	}
+	
 	private static void  case4() {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		
-		driver.
-		WebElement field1 = driver.findElement(By.xpath("//*[id()='edit-rechercher']"))
-		field1. write( "Donald Trump" );
-		WebElement button2 = driver.findElement(By.xpath("//*[text()='Appliquer les filtres']"))
+		driver.get("http://www.imt-atlantique.fr/fr/rechercher");
+		      		
+		new WebDriverWait(driver, 20).until(ExpectedConditions.visibilityOfElementLocated(
+			By.xpath("//button[@class='agree-button eu-cookie-compliance-default-button']"))).click();
+			
+		WebElement field1 = driver.findElements(By.xpath("//*[id()='edit-rechercher']")).get(0);
+		field1.sendKeys("Donald Trump");
+		WebElement button2 = driver.findElements(By.xpath("//*[text()='Appliquer les filtres']")).get(0);
 		button2.click();
-		WebElement div3 = driver.findElement(By.xpath("//*[text()='Toutes les actualités']"));
-		Assert.assertNotNull(div);
+		WebElement div3 = driver.findElements(By.xpath("//*[text()='Toutes les actualités']")).get(0);
+		Assert.assertNotNull(div3);
+		
 		driver.close();
 	}
 	private static void  case5() {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		
-		driver.
-		WebElement checkbox1 = driver.findElement(By.tagName("*"))
-		It's some string.
-		WebElement checkbox2 = driver.findElement(By.xpath("//*[label()='Anglais']"))
-		It's some string.
-		WebElement checkbox3 = driver.findElement(By.xpath("//*[label()='A domicile']"))
-		It's some string.
-		WebElement checkbox4 = driver.findElement(By.xpath("//*[label()='Temps plein']"))
-		It's some string.
-		WebElement button5 = driver.findElement(By.xpath("//*[text()='Appliquer les critéres']"))
+		driver.get("http://www.imt-atlantique.fr/fr/formation/trouver-ma-formation");
+		      		
+		new WebDriverWait(driver, 20).until(ExpectedConditions.visibilityOfElementLocated(
+			By.xpath("//button[@class='agree-button eu-cookie-compliance-default-button']"))).click();
+			
+		WebElement checkbox1 = driver.findElements(By.tagName("*")).get(0);
+		if ( checkbox1.isSelected() ){checkbox1.click();}
+		WebElement checkbox2 = driver.findElements(By.xpath("//*[label()='Anglais']")).get(0);
+		if ( !checkbox2.isSelected() ){checkbox2.click();}
+		WebElement checkbox3 = driver.findElements(By.xpath("//*[label()='A domicile']")).get(0);
+		if ( !checkbox3.isSelected() ){checkbox3.click();}
+		WebElement checkbox4 = driver.findElements(By.xpath("//*[label()='Temps plein']")).get(0);
+		if ( !checkbox4.isSelected() ){checkbox4.click();}
+		WebElement button5 = driver.findElements(By.xpath("//*[text()='Appliquer les critéres']")).get(0);
 		button5.click();
-		WebElement div6 = driver.findElement(By.xpath("//*[text()='No found course matching your criteria.']"));
-		Assert.assertNotNull(div);
+		WebElement div6 = driver.findElements(By.xpath("//*[text()='No found course matching your criteria.']")).get(0);
+		Assert.assertNotNull(div6);
+		
 		driver.close();
 	}
 	private static void  case6() {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		
-		driver.
-		WebElement field1 = driver.findElement(By.xpath("//*[label()='Rechercher']"))
-		field1. write( "2007" );
-		WebElement combobox2 = driver.findElement(By.xpath("//*[label()='Période de publication']"))
-		It's some string.
-		WebElement button3 = driver.findElement(By.xpath("//*[text()='Appliquer les filtres']"))
+		driver.get("http://www.imt-atlantique.fr/fr/rechercher");
+		      		
+		new WebDriverWait(driver, 20).until(ExpectedConditions.visibilityOfElementLocated(
+			By.xpath("//button[@class='agree-button eu-cookie-compliance-default-button']"))).click();
+			
+		WebElement field1 = driver.findElements(By.xpath("//*[label()='Rechercher']")).get(0);
+		field1.sendKeys("2007");
+		WebElement combobox2 = driver.findElements(By.xpath("//*[label()='Période de publication']")).get(0);
+		combobox2.click();
+		WebElement button3 = driver.findElements(By.xpath("//*[text()='Appliquer les filtres']")).get(0);
 		button3.click();
-		WebElement div4 = driver.findElement(By.xpath("//*[text()='Aucun résultat ne correspond à votre recherche.']"));
-		Assert.assertNotNull(div);
-		driver.close();
-	}
-	private static void  case7() {
-		WebDriver driver = new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		WebElement div4 = driver.findElements(By.xpath("//*[text()='Aucun résultat ne correspond à votre recherche.']")).get(0);
+		Assert.assertNotNull(div4);
 		
-		driver.
-		div myTitle = WebElement div1 = driver.findElement(By.xpath("//*[@class()='actu_home_ctner_inner_cell1_titre']"));
-		WebElement div2 = driver.findElement(By.xpath("//*[@class()='actu_home_ctner_inner_cell1_titre']"))
-		div2.click();
-		WebElement title3 = driver.findElement(By.xpath("//*[text()='null']"));
-		Assert.assertNotNull(title);
 		driver.close();
 	}
-	private static void  case8() {
-		WebDriver driver = new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-		
-		driver.
-		div myTitle = WebElement div1 = driver.findElement(By.xpath("//*[@class()='actu_home_ctner_inner_cell1_titre']"));
-		div myUrl = WebElement div2 = driver.findElement(By.xpath("//*[@class()='actu_home_ctner_inner_cell1_titre']"));
-		driver.
-		WebElement field3 = driver.findElement(By.xpath("//*[id()='edit-rechercher']"))
-		field3. write(myTitle);
-		WebElement button4 = driver.findElement(By.xpath("//*[text()='Appliquer les filtres']"))
-		button4.click();
-		WebElement link5 = driver.findElement(By.xpath("//a[href()='null']"));
-		Assert.assertNotNull(link);
-		driver.close();
-	}
-	
 	
 }
--- SeleniumWebdriverTests/src-gen/TestokTest.java
+++ SeleniumWebdriverTests/src-gen/TestokTest.java
@@ -6,29 +6,117 @@ public class testokTest {
 		
 		case1();
 		case2();
+		case3();
+		case7();
+		case8();
 					
-	} 
+	}
 	
+		
 	private static void  case1() {
+		boolean cookiesAlreadyChecked = false;
 		WebDriver driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		
-		driver.get( "http://www.imt-atlantique.fr/fr" );
-		WebElement link1 = driver.findElement(By.xpath("//a[text()='Toutes les actualités']"));;
+		driver.get("http://www.imt-atlantique.fr/fr");
+		if(!cookiesAlreadyChecked) {
+				new WebDriverWait(driver, 20).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@class='agree-button eu-cookie-compliance-default-button']"))).click(); //ACCEPT COOKIE
+				cookiesAlreadyChecked = true;
+		}
+		
+		WebElement link1 = driver.findElements(By.partialLinkText(new String("Toutes les actualités").toUpperCase())).get(0);
 		Assert.assertNotNull(link1);
+		
 		driver.close();
 	}
+		
 	private static void  case2() {
+		boolean cookiesAlreadyChecked = false;
 		WebDriver driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		
-		driver.get( "http://www.imt-atlantique.fr/fr" );
-		WebElement link1 = driver.findElement(By.xpath("//a[text()='Toutes les actualités']"));
+		driver.get("http://www.imt-atlantique.fr/fr");
+		if(!cookiesAlreadyChecked) {
+				new WebDriverWait(driver, 20).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@class='agree-button eu-cookie-compliance-default-button']"))).click(); //ACCEPT COOKIE
+				cookiesAlreadyChecked = true;
+		}
+		
+		WebElement link1 = driver.findElements(By.partialLinkText(new String("Toutes les actualités").toUpperCase())).get(0);
 		link1.click();
-		WebElement link2 = driver.findElement(By.xpath("//a[text()='Accueil']"));;
+		WebElement link2 = driver.findElements(By.partialLinkText(new String("Accueil").toUpperCase())).get(0);
 		Assert.assertNotNull(link2);
+		
 		driver.close();
 	}
-	
+		
+	private static void  case3() {
+		boolean cookiesAlreadyChecked = false;
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		
+		driver.get("http://www.imt-atlantique.fr/fr");
+		if(!cookiesAlreadyChecked) {
+				new WebDriverWait(driver, 20).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@class='agree-button eu-cookie-compliance-default-button']"))).click(); //ACCEPT COOKIE
+				cookiesAlreadyChecked = true;
+		}
+		
+		WebElement link1 = driver.findElements(By.partialLinkText(new String("Toutes les actualités").toUpperCase())).get(0);
+		link1.click();
+		WebElement image2 = driver.findElements(By.xpath("//*[@alt='Accueil']")).get(0);
+		image2.click();
+		WebElement link3 = driver.findElements(By.partialLinkText(new String("Toutes les actualités").toUpperCase())).get(0);
+		Assert.assertNotNull(link3);
+		
+		driver.close();
+	}
+		
+	private static void  case7() {
+		boolean cookiesAlreadyChecked = false;
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		
+		driver.get("http://www.imt-atlantique.fr/fr");
+		if(!cookiesAlreadyChecked) {
+				new WebDriverWait(driver, 20).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@class='agree-button eu-cookie-compliance-default-button']"))).click(); //ACCEPT COOKIE
+				cookiesAlreadyChecked = true;
+		}
+		
+		String myTitle = driver.findElements(By.className("actu_home_ctner_inner_cell1_titre")).get(0).getText();
+		WebElement div1 = driver.findElements(By.className("actu_home_ctner_inner_cell1_titre")).get(0);
+		div1.click();
+		WebElement title2 = driver.findElement(By.xpath("//title"));
+		Assert.assertTrue(title2.getText().equals(myTitle));
+		
+		driver.close();
+	}
+		
+	private static void  case8() {
+		boolean cookiesAlreadyChecked = false;
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		
+		driver.get("http://www.imt-atlantique.fr/fr");
+		if(!cookiesAlreadyChecked) {
+				new WebDriverWait(driver, 20).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@class='agree-button eu-cookie-compliance-default-button']"))).click(); //ACCEPT COOKIE
+				cookiesAlreadyChecked = true;
+		}
+		
+		String myTitle = driver.findElements(By.className("actu_home_ctner_inner_cell1_titre")).get(2).getText();
+		WebElement myUrl = driver.findElements(By.className("actu_home_ctner_inner_cell1_titre")).get(2);
+		driver.get("http://www.imt-atlantique.fr/fr/rechercher");
+		if(!cookiesAlreadyChecked) {
+				new WebDriverWait(driver, 20).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@class='agree-button eu-cookie-compliance-default-button']"))).click(); //ACCEPT COOKIE
+				cookiesAlreadyChecked = true;
+		}
+		
+		WebElement field1 = driver.findElements(By.xpath("//input[id()='edit-search-api-fulltext']")).get(0);
+		field1.sendKeys(myTitle);
+		WebElement button2 = driver.findElements(By.xpath("//button[text()='Appliquer les filtres']")).get(0);
+		button2.click();
+		WebElement link3 = driver.findElements(By.partialLinkText(new String("" + myUrl+" ").toUpperCase())).get(0);
+		Assert.assertNotNull(link3);
+		
+		driver.close();
+	}
 	
 }
